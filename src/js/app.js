9b45474eb59a6dd86cfff16911a763fc9cc40bdf
William Hibberd <will@williamhibberd.co.uk>
2022-11-07 21:10:28 +0000

    added auto scrolling

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,12 @@
 import Alpine from "alpinejs";
-import lazySizes from "lazysizes";
+import "lazysizes";
 
-import "./utils/vh-screen";
 import "../css/app.pcss";
-import document from "./components/document";
+import "../js/utils/vh-screen";
+import "../js/components/document";
 
 // only needed to get apline dev tools working
 window.Alpine = Alpine;
 
-// Alpine plugins
-
-// Alpine data
-Alpine.data("document", document);
-
 // Alpine start
 Alpine.start();
-init();
-
-/* Define init function */
-function init() {
-	vhFix();
-}
